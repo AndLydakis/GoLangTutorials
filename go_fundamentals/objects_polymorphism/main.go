f88b2e652f99ac44e0b1ae1571581933a73f6dcd
Andreas Lydakis <andlydakis@gmail.com>
2024-10-19 20:03:20 +0200

    generics

--- go_fundamentals/objects_polymorphism/main.go
+++ go_fundamentals/objects_polymorphism/main.go
@@ -1,36 +1,64 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"objects_poly/menu"
-	"os"
-	"strings"
-)
+import "fmt"
 
 func main() {
-
-loop:
-	for {
-		fmt.Println("Select an option")
-		fmt.Println("1) Print Menu")
-		fmt.Println("2) Add Item")
-		fmt.Println("q) Quit")
-		var in = bufio.NewReader(os.Stdin)
-		choice, _ := in.ReadString('\n')
-		choice = strings.TrimSpace(choice)
-
-		switch choice {
-		case "1":
-			menu.PrintMenu()
-		case "2":
-			menu.AddItem()
-		case "q":
-			fmt.Println("Quit")
-			break loop
-		default:
-			break loop
-		}
+	testScores := []float32{
+		87.3,
+		105,
+		63.5,
+		257,
 	}
 
+	testScores2 := map[string]float64{
+		"A": 87.3,
+		"B": 105,
+		"C": 63.5,
+		"D": 257,
+	}
+
+	c := clone(testScores)
+	fmt.Println(&testScores[0], &c[0])
+
+	d := cloneM(testScores2)
+	fmt.Println(d)
+
+	a1 := []int{1, 2, 3}
+	a2 := []float64{3.14, 6.02}
+	a3 := []string{"foo", "bar", "baz"}
+	s1 := add(a1)
+	s2 := add(a2)
+	s3 := add(a3)
+
+	fmt.Printf("Sum of %v: %v\n", a1, s1)
+	fmt.Printf("Sum of %v: %v\n", a2, s2)
+	fmt.Printf("Sum of %v: %v\n", a3, s3)
+}
+
+type addable interface {
+	int | float64 | string
+}
+
+func add[V addable](s []V) V {
+	var result V
+	for _, v := range s {
+		result += v
+	}
+	return result
+}
+
+func clone[V any](s []V) []V {
+	result := make([]V, len(s))
+	for i, v := range s {
+		result[i] = v
+	}
+	return result
+}
+
+func cloneM[K comparable, V any](m map[K]V) map[K]V {
+	result := make(map[K]V, len(m))
+	for k, v := range m {
+		result[k] = v
+	}
+	return result
 }
